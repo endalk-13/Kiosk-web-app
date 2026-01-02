c0427a78e60c8fd9e6a35dcd49bcded7fb7d7120
endalk <endalkfassil88@gmail.com>
2026-01-02 17:18:00 +0300

    Added logo and navbar menu

--- Kiosk-frontend/src/navbar/Navbar.jsx
+++ Kiosk-frontend/src/navbar/Navbar.jsx
@@ -1,1 +1,26 @@
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.jpg";
+import { Link } from "react-router-dom";
 
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-logo-wrapper">
+          <img src={logo} alt="Company Logo" className="navbar-logo" />
+        </div>
+
+        <ul className="navbar-menu">
+            <li className="navbar-item"><Link to="/">Home</Link></li>
+            <li className="navbar-item"><Link to="/cart">Cart</Link></li>
+            <li className="navbar-item"><Link to="/orders">Check-out</Link></li>
+        </ul>
+
+       
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
