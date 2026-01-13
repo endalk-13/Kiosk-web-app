d685da03f716347ee5626aaf602fab76c3f6f829
Rohobot Getu <rohobotgetu@192.168.1.3>
2026-01-13 13:37:22 +0300

    added cart.java and cartservice.java

--- Kiosk-backend/kiosk/kiosk/src/main/java/com/example/kiosk/Service/cartService.java
+++ Kiosk-backend/kiosk/kiosk/src/main/java/com/example/kiosk/Service/cartService.java
@@ -1,5 +1,26 @@
 package com.example.kiosk.Service;
 
+import com.example.kiosk.Models.Cart;
+import org.springframework.stereotype.Service;
+
+@Service
 public class cartService {
-    
+
+    private final Cart cart = new Cart();
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void addItem(String item, double price) {
+        cart.addItem(item, price);
+    }
+
+    public void removeItem(String item, double price) {
+        cart.removeItem(item, price);
+    }
+
+    public void clearCart() {
+        cart.clearCart();
+    }
 }
