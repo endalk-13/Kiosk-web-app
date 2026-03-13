d80a6a74d02a41b47d18b9f3777a5f8b70beaa92
endalk <endalkfassil88@gmail.com>
2026-03-13 16:03:02 +0300

    updated app . jsx

--- Kiosk-frontend/src/App.jsx
+++ Kiosk-frontend/src/App.jsx
@@ -2,21 +2,25 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./navbar/Navbar";
 import Home from "./homepage/homepage";
+import Cart from "./cart/cart";
+import Checkout from "./payment/checkout";
+import Kitchen from "./kitchen/kitchen";
 
+import { StoreProvider } from "./StoreContext";
 
 function App() {
-  
-
   return (
-    <>
+    <StoreProvider>
       <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
-    </>
-  )
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/kitchen" element={<Kitchen />} />
+        </Routes>
+      </Router>
+    </StoreProvider>
+  );
 }
-
 export default App
