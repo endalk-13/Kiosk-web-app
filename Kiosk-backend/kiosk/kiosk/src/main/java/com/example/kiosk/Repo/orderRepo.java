7bf38d7af19d692040bcb4578fc7b7fcc0c7e5f2
endalk <endalkfassil88@gmail.com>
2026-07-10 18:30:23 +0300

    updated orderrepo

--- Kiosk-backend/kiosk/kiosk/src/main/java/com/example/kiosk/Repo/orderRepo.java
+++ Kiosk-backend/kiosk/kiosk/src/main/java/com/example/kiosk/Repo/orderRepo.java
@@ -1,5 +1,14 @@
 package com.example.kiosk.Repo;
 
-public interface orderRepo {
-    
+import com.example.kiosk.Models.Order;
+import com.example.kiosk.Models.OrderStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface orderRepo extends JpaRepository<Order, Long> {
+
+    List<Order> findByStatusInOrderByCreatedAtAsc(List<OrderStatus> statuses);
+
+    List<Order> findByStatusOrderByCreatedAtDesc(OrderStatus status);
 }
