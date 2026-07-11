f55cdaa0dca337b43a3e3b69d8eab1e75d0260f2
endalk <endalkfassil88@gmail.com>
2026-07-11 22:33:37 +0300

    Used navigate instead of windows . location . href

--- Kiosk-frontend/src/landingpage.jsx
+++ Kiosk-frontend/src/landingpage.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
+  const navigate = useNavigate();
+
   return (
     <div className="landing-page" style={{
       display: "flex",
@@ -11,13 +13,13 @@ function LandingPage() {
       minHeight: "100vh",
       minWidth: "100vw",
       backgroundColor: "#FF6B35",
-        color: "white",
-        textAlign: "center",
-        padding: "20px",
+      color: "white",
+      textAlign: "center",
+      padding: "20px",
     }}>
       <h1>Welcome to Burger House Kiosk system</h1>
       <p>Order your favorite burgers with ease!</p>
-      <button onClick={() => window.location.href = "/Home"}>
+      <button onClick={() => navigate("/home")}>
         Start Ordering
       </button>
     </div>
